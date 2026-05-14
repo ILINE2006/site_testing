1c370f660b82c971b4be2d3c3c2438c7a08ac07c
ILINE2006 <teplotom71@gmail.com>
2026-05-15 02:21:51 +0700

    Модуль Личный кабинет: просмотр уведомлений пользователя, прочтение уведомлений пользователя

--- cypress/e2e/habr_cypress_test.cy.js
+++ cypress/e2e/habr_cypress_test.cy.js
@@ -15,10 +15,26 @@ describe('Cypress Tests', () => {
     cy.get('.form-input--text').type('testerAdmin');
     cy.get('.form-input--password').type('Password1');
     cy.get(':nth-child(3) > .button', {timeout: 3000}).click();
+    cy.get('[data-v-7ff322b7=""][data-v-f5840952=""] > .button').click();
 
 
   })
 });
 
+it('Уведомления', () => {
+  cy.fixture('cypressTests').then(data => {
+  cy.visit(data.main_url);
+  cy.get('[href="/login"] > .button').click();
+  cy.get('.form-input--text').type('testerAdmin');
+  cy.get('.form-input--password').type('Password1');
+  cy.get(':nth-child(3) > .button', {timeout: 3000}).click();
+  cy.get('[href="/notification"] > .header__label').click();
+  cy.get('.notification-title > .link', {timeout: 6000}).click();
+  cy.get('[data-v-7ff322b7=""][data-v-f5840952=""] > .button', {timeout: 3000}).click();
   
+
+
+})
+});
+
 });
