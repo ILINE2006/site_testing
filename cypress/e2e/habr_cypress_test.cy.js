033ab955a46b448ab59b92248dc31466d588cf1f
ILINE2006 <teplotom71@gmail.com>
2026-05-15 10:20:21 +0700

    Модуль заявки реализован

--- cypress/e2e/habr_cypress_test.cy.js
+++ cypress/e2e/habr_cypress_test.cy.js
@@ -28,8 +28,10 @@ describe('Cypress Tests', () => {
     cy.fixture('cypressTests').then(data => {
     cy.visit(data.main_url);
     cy.get(':nth-child(1) > .header__nav > [href="/vacancies"] > .header__label', {timeout: 80000}).click();
+    cy.wait(10000);
     cy.get('[href="/login"] > .button', {timeout: 100000}).click();
-    cy.get('.form-input--text', {timeout: 80000}).type('seven');
+    cy.wait(10000);
+    cy.get('.form-input--text', {timeout: 80000}).type('testerAdmin');
     cy.get('.form-input--password', {timeout: 80000}).type('Password1');
     cy.get(':nth-child(3) > .button', {timeout: 1000000}).click();
     cy.get('.page-nav__role-block > .button').click();
@@ -46,7 +48,39 @@ describe('Cypress Tests', () => {
     cy.get('.form-input--password').type('Password1');
     cy.get(':nth-child(3) > .button', {timeout: 3000}).click();
     cy.get('[data-v-7ff322b7=""][data-v-f5840952=""] > .button').click();
+
+
   })
+});
+
+it('Заявки', () => {
+  cy.fixture('cypressTests').then(data => {
+  cy.visit(data.main_url);
+  cy.get('[href="/login"] > .button').click();
+  cy.get('.form-input--text').type('six');
+  cy.get('.form-input--password').type('Password1');
+  cy.get(':nth-child(3) > .button', {timeout: 3000}).click();
+  cy.get('.page-nav__role-block > .button').click();
+  cy.get('.select-role-form > :nth-child(2)').click();
+  cy.get('.variants-company > :nth-child(2)').click();
+  cy.get(':nth-child(1) > .form-control--medium > .form-input--text').type('testerAdmin');
+  cy.get(':nth-child(2) > .form-control--medium > .form-input--text').type('testerAdmin');
+  cy.get('.form-area').type('testerAdmin');
+  cy.get('.create-company-form__description-block > .button', {timeout: 3000}).click();
+  cy.get('[data-v-7ff322b7=""][data-v-f5840952=""] > .button', {timeout: 3000}).click();
+  cy.get('[href="/login"] > .button').click();
+  cy.get('.form-input--text').type('sixseven');
+  cy.get('.form-input--password').type('Password1');
+  cy.get(':nth-child(3) > .button', {timeout: 3000}).click();
+  cy.get('.page-nav__role-block > .button').click();
+  cy.get('.select-role-form > :nth-child(1)').click();
+  cy.get('.variants-company > :nth-child(2)').click();
+  cy.get(':nth-child(1) > .form-control--medium > .form-input--text').type('testerAdmin');
+  cy.get(':nth-child(2) > .form-control--medium > .form-input--text').type('testerAdmin');
+  cy.get('.form-area').type('testerAdmin');
+  cy.get('.create-company-form__description-block > .button', {timeout: 3000}).click();
+  cy.get('[data-v-7ff322b7=""][data-v-f5840952=""] > .button').click();
+})
 });
 
 it('Уведомления', () => {
